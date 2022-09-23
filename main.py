855899e95c9f0e4f97837aa70b484425f3785956
IslamOuda2004 <113916264+IslamOuda2004@users.noreply.github.com>
2022-09-23 21:06:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+number = input('enter the anything')
+arr = []
+
+counter = 0
+
+for var in number:
+    arr.append(var)
+
+operation = arr.reverse()
+
+result =''
+
+for work in arr:
+    result += work
+
+print(result)
+
+
+
